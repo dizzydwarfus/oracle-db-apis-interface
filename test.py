186a6ab49d7d27cd372585fe262787595c599da0
dizzydwarfus <lianzhenyang1998@gmail.com>
2024-08-14 00:10:38 +0200

    added batchload via csv files to items staging table; added error message logging on load_items procedure

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
+# %%
 import requests
 from dotenv import load_dotenv
 import os
 import json
+import pandas as pd
+
+from _globals import PRODUCT_COLUMN_MAPPING
 
 load_dotenv()
 
@@ -9,6 +13,31 @@ auth_details = {
     "url": os.getenv("APEX_ORACLE_DB_URL"),
     "client_id": os.getenv("APEX_ORACLE_CLIENT_ID"),
     "client_password": os.getenv("APEX_ORACLE_CLIENT_SECRET"),
+}
+
+endpoints = {
+    "insert_product": {
+        "url": "/products/{product_id}",
+        "method": "GET",
+        "headers": {"Authorization": "Bearer {access_token}"},
+    },
+    "insert_products": {
+        "url": "/products/insert_staging",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+        },
+        "data": "data_to_insert.json",
+    },
+    "batch_load_products": {
+        "url": "/items_staging/batchload",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "text/csv",
+            "Authorization": "Bearer {access_token}",
+        },
+        "data": "data_to_insert.csv",
+    },
 }
 
 
@@ -49,15 +78,70 @@ def insert_product(auth: OracleAuthentication, data_to_insert: dict):
     )
     return data
 
-def load_items(auth: OracleAuthentication):
-    query_url = f"{auth.base_url}/products/load_items"
-    data = requests.post(
+
+def get_all_products(auth: OracleAuthentication, product_id: int = None):
+    product_id = "" if product_id is None else product_id
+    query_url = f"{auth.base_url}/items_staging/{product_id}"
+    data = requests.get(
         query_url,
         headers={"Authorization": f"Bearer {auth.access_token}"},
     )
-    return data
+    return data.json()
 
-with open("data_to_insert.json", "r") as file:
-    data_to_insert = json.loads(file.read())
 
-# insert_product(auth, data_to_insert)
+def batch_load_products(auth: OracleAuthentication, csv_file_path: str):
+    insert_url = f"{auth.base_url}/items_staging/batchload"
+    with open(csv_file_path, "rb") as csv_file:
+        data = csv_file.read()
+        response = requests.post(
+            insert_url,
+            headers={
+                "Authorization": f"Bearer {auth.access_token}",
+                "Content-Type": "text/csv",
+            },
+            data=data,
+        )
+    return response
+
+
+# TODO: need to customize the procedure in oracle to return error messages/response
+def load_items(auth: OracleAuthentication):
+    query_url = f"{auth.base_url}/products/load_items"
+    response = requests.post(
+        query_url,
+        headers={"Authorization": f"Bearer {auth.access_token}"},
+    )
+    return response
+
+
+def parse_load_items_error(response):
+    result = json.loads(response.json()["result"])
+    return result
+
+
+def check_load_items_status(result):
+    return result["status"] == "success"  # return True if success else False
+
+
+# use pydantic to validate csv or json data to make sure column names and data types are correct
+
+# %%
+csv_data = "product_mock_for_apex.csv"
+formatted_csv_data = "product_mock_for_apex_formatted.csv"
+df = pd.read_csv(csv_data)
+df = df.rename(columns=PRODUCT_COLUMN_MAPPING)
+df = df.drop(columns=["ZITA_CATEGORY"])
+df = df.fillna("")
+df["ID"] = ""  # ID is not a nullable column in DB so must be in the csv even if blank
+df.to_csv(
+    formatted_csv_data, index=False, encoding="utf-8"
+)  # only encoding utf-8 is supported
+
+response = batch_load_products(auth=auth, csv_file_path=formatted_csv_data)
+print(response.text)
+
+# %%
+load_execution_response = load_items(auth)
+result = parse_load_items_error(load_execution_response)
+print(check_load_items_status(result))
+# %%
